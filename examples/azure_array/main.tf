4ba7b040a1d4767e527243003feffb41069a3535
David Stamen <dstamen@gmail.com>
2021-12-07 09:38:12 -0800

    update provider version

--- examples/azure_array/main.tf
+++ examples/azure_array/main.tf
@@ -2,7 +2,7 @@ terraform {
     required_providers {
         cbs = {
             source = "PureStorage-OpenConnect/cbs"
-            version = "0.2.0"
+            version = "~> 0.6.0"
         }
     }
 }
