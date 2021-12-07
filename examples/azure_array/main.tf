059126c9714263d85c9a3498b1be709b1fab3d9e
David Stamen <dstamen@gmail.com>
2021-12-07 09:37:20 -0800

    add outputs

--- examples/azure_array/main.tf
+++ examples/azure_array/main.tf
@@ -42,3 +42,25 @@ resource "cbs_array_azure" "azure_instance" {
         version = var.plan_version
     }
 }
+
+output "cbs_mgmt_endpoint" {
+    value = cbs_array_azure.azure_instance.management_endpoint
+}
+output "cbs_mgmt_endpoint_ct0" {
+    value = cbs_array_azure.azure_instance.management_endpoint_ct0
+}
+output "cbs_mgmt_endpoint_ct1" {
+    value = cbs_array_azure.azure_instance.management_endpoint_ct1
+}
+output "cbs_repl_endpoint_ct0" {
+    value = cbs_array_azure.azure_instance.replication_endpoint_ct0
+}
+output "cbs_repl_endpoint_ct1" {
+    value = cbs_array_azure.azure_instance.replication_endpoint_ct1
+}
+output "cbs_iscsi_endpoint_ct0" {
+    value = cbs_array_azure.azure_instance.iscsi_endpoint_ct0
+}
+output "cbs_iscsi_endpoint_ct1" {
+    value = cbs_array_azure.azure_instance.iscsi_endpoint_ct1
+}
